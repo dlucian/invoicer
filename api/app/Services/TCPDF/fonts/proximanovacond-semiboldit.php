76df9d9f30e8eda71a2a10db15590a4c14132982
Lucian Daniliuc <dlucian@gmail.com>
2015-09-16 09:36:11 +0300

    API PDF fonts

---
+++ api/app/Services/TCPDF/fonts/proximanovacond-semiboldit.php
@@ -0,0 +1,24 @@
+<?php
+$type='TrueType';
+$name='ProximaNovaCond-SemiboldIt';
+$desc=array('Ascent'=>667,'Descent'=>-184,'CapHeight'=>667,'Flags'=>32,'FontBBox'=>'[-189 -273 974 886]','ItalicAngle'=>0,'StemV'=>120,'MissingWidth'=>479);
+$up=-113;
+$ut=20;
+$cw=array(
+	chr(0)=>479,chr(1)=>479,chr(2)=>479,chr(3)=>479,chr(4)=>479,chr(5)=>479,chr(6)=>479,chr(7)=>479,chr(8)=>479,chr(9)=>479,chr(10)=>479,chr(11)=>479,chr(12)=>479,chr(13)=>479,chr(14)=>479,chr(15)=>479,chr(16)=>479,chr(17)=>479,chr(18)=>479,chr(19)=>479,chr(20)=>479,chr(21)=>479,
+	chr(22)=>479,chr(23)=>479,chr(24)=>479,chr(25)=>479,chr(26)=>479,chr(27)=>479,chr(28)=>479,chr(29)=>479,chr(30)=>479,chr(31)=>479,' '=>211,'!'=>242,'"'=>373,'#'=>524,'$'=>489,'%'=>663,'&'=>538,'\''=>211,'('=>257,')'=>257,'*'=>330,'+'=>441,
+	','=>243,'-'=>270,'.'=>242,'/'=>265,'0'=>528,'1'=>352,'2'=>510,'3'=>498,'4'=>493,'5'=>514,'6'=>513,'7'=>450,'8'=>509,'9'=>513,':'=>238,';'=>242,'<'=>441,'='=>441,'>'=>441,'?'=>390,'@'=>703,'A'=>538,
+	'B'=>541,'C'=>549,'D'=>584,'E'=>473,'F'=>464,'G'=>578,'H'=>595,'I'=>238,'J'=>410,'K'=>536,'L'=>429,'M'=>714,'N'=>594,'O'=>615,'P'=>515,'Q'=>615,'R'=>535,'S'=>488,'T'=>464,'U'=>593,'V'=>538,'W'=>747,
+	'X'=>538,'Y'=>524,'Z'=>477,'['=>229,'\\'=>265,']'=>229,'^'=>386,'_'=>422,'`'=>203,'a'=>495,'b'=>495,'c'=>417,'d'=>495,'e'=>476,'f'=>273,'g'=>495,'h'=>486,'i'=>217,'j'=>217,'k'=>457,'l'=>217,'m'=>723,
+	'n'=>486,'o'=>490,'p'=>494,'q'=>494,'r'=>308,'s'=>402,'t'=>282,'u'=>486,'v'=>432,'w'=>669,'x'=>426,'y'=>432,'z'=>400,'{'=>244,'|'=>196,'}'=>244,'~'=>458,chr(127)=>479,chr(128)=>479,chr(129)=>479,chr(130)=>479,chr(131)=>479,
+	chr(132)=>479,chr(133)=>479,chr(134)=>479,chr(135)=>479,chr(136)=>479,chr(137)=>479,chr(138)=>479,chr(139)=>479,chr(140)=>479,chr(141)=>479,chr(142)=>479,chr(143)=>479,chr(144)=>479,chr(145)=>479,chr(146)=>479,chr(147)=>479,chr(148)=>479,chr(149)=>479,chr(150)=>479,chr(151)=>479,chr(152)=>479,chr(153)=>479,
+	chr(154)=>479,chr(155)=>479,chr(156)=>479,chr(157)=>479,chr(158)=>479,chr(159)=>479,chr(160)=>211,chr(161)=>538,chr(162)=>298,chr(163)=>447,chr(164)=>522,chr(165)=>429,chr(166)=>488,chr(167)=>406,chr(168)=>278,chr(169)=>484,chr(170)=>488,chr(171)=>464,chr(172)=>477,chr(173)=>270,chr(174)=>477,chr(175)=>477,
+	chr(176)=>300,chr(177)=>495,chr(178)=>168,chr(179)=>266,chr(180)=>203,chr(181)=>310,chr(182)=>402,chr(183)=>251,chr(184)=>195,chr(185)=>402,chr(186)=>402,chr(187)=>297,chr(188)=>400,chr(189)=>288,chr(190)=>400,chr(191)=>400,chr(192)=>535,chr(193)=>538,chr(194)=>538,chr(195)=>538,chr(196)=>538,chr(197)=>429,
+	chr(198)=>549,chr(199)=>549,chr(200)=>549,chr(201)=>473,chr(202)=>473,chr(203)=>473,chr(204)=>473,chr(205)=>238,chr(206)=>238,chr(207)=>584,chr(208)=>608,chr(209)=>594,chr(210)=>594,chr(211)=>615,chr(212)=>615,chr(213)=>615,chr(214)=>615,chr(215)=>441,chr(216)=>535,chr(217)=>593,chr(218)=>593,chr(219)=>593,
+	chr(220)=>593,chr(221)=>524,chr(222)=>464,chr(223)=>557,chr(224)=>308,chr(225)=>495,chr(226)=>495,chr(227)=>495,chr(228)=>495,chr(229)=>217,chr(230)=>417,chr(231)=>417,chr(232)=>417,chr(233)=>476,chr(234)=>476,chr(235)=>476,chr(236)=>476,chr(237)=>217,chr(238)=>217,chr(239)=>590,chr(240)=>503,chr(241)=>486,
+	chr(242)=>486,chr(243)=>490,chr(244)=>490,chr(245)=>490,chr(246)=>490,chr(247)=>443,chr(248)=>308,chr(249)=>486,chr(250)=>486,chr(251)=>486,chr(252)=>486,chr(253)=>432,chr(254)=>282,chr(255)=>141);
+$enc='iso-8859-2';
+$diff='128 /.notdef 130 /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef 142 /.notdef 145 /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef 158 /.notdef /.notdef 161 /Aogonek /breve /Lslash 165 /Lcaron /Sacute 169 /Scaron /Scedilla /Tcaron /Zacute 174 /Zcaron /Zdotaccent 177 /aogonek /ogonek /lslash 181 /lcaron /sacute /caron 185 /scaron /scedilla /tcaron /zacute /hungarumlaut /zcaron /zdotaccent /Racute 195 /Abreve 197 /Lacute /Cacute 200 /Ccaron 202 /Eogonek 204 /Ecaron 207 /Dcaron /Dcroat /Nacute /Ncaron 213 /Ohungarumlaut 216 /Rcaron /Uring 219 /Uhungarumlaut 222 /Tcommaaccent 224 /racute 227 /abreve 229 /lacute /cacute 232 /ccaron 234 /eogonek 236 /ecaron 239 /dcaron /dcroat /nacute /ncaron 245 /ohungarumlaut 248 /rcaron /uring 251 /uhungarumlaut 254 /tcommaaccent /dotaccent';
+$file='ProximaNovaCond-SemiboldIt.z';
+$originalsize=95692;
+?>
